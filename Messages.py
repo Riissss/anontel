e9b53ad7c317ac510388bc6910044a26f45cc6a3
Riissss <121730144+Riissss@users.noreply.github.com>
2023-01-01 13:01:40 +0700

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,46 @@
+m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Semoga Dapat teman atau jodoh\n\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
+
+m_is_not_free_users = (
+    "🙆⚠ Anda tidak dalam percakapan, ketik /start untuk memulai, atau /help untuk bantuan.\n\n💌TEKAN TOMBOL 🇮🇩NEW CHAT🇮🇩"
+)
+
+m_is_connect = "Pasangan telah ditemukan!
+Katakan, Hai untuk membalas pasangan anda 😊 
+
+/next - Untuk mencari obrolan baru
+/stop - Untuk berhenti dari obrolan
+
+🔊 PROMOTE 🔊
+
+Yuk bikin pesan rahasia pakai bot ini:
+👉 @rp_rmb_bot"
+
+m_play_again = "Apakah Anda Ingin mengobrol dengan orang lain?\n\nDo you want to chat with other people?"
+
+m_is_not_user_name = "❌ Maaf, bot kami hanya dapat berkomunikasi jika Anda memiliki nama pengguna\n\n❌ Sorry, our bot can only communicate if you have a username"
+
+m_good_bye = "❌Pasangan anda menutup percakapan, ketik /start untuk memulai percakapan lain.️\n\n❌ Sorry your interlocutor left the chat"
+
+m_disconnect_user = "Maaf sambungan telah terputus!\n\nSorry, the connection was lost!"
+
+m_failed = "❌ Terjadi kesalahan!\n\n❌ Loss happened"
+
+m_like = "✅ Anda mengerimkan username kepada lawan bicara!\n\n✅ You send the username to the other person"
+
+m_dislike_user = "Obrolan Telah Berakhir\n\nChat Has Ended\n\n/next"
+
+m_dislike_user_to = (
+    "⛔ Lawan bicara anda meninggalkan obrolan\n\n⛔ Your interlocutor leaves the chat"
+)
+
+m_send_some_messages = "Bot tidak bisa meneruskan pesan dari bot\n\nThe bot could not continue the pan from the bot"
+
+m_has_not_dialog = "⚠ Anda tidak dalam percakapan, ketik /start untuk memulai, atau /help untuk bantuan.\n\nYou are not in chat"
+
+dislike_str = "⛔ Exit"
+
+like_str = "📠 SEND ID"
+
+
+def m_all_like(x):
+    return "Nih.. Chat ajah yah " + "username : @" + str(x)
---
+++ README.md
@@ -0,0 +1,51 @@
+# TelegramChat
+
+
+<p align="center">
+  <a href="https://github.com/Dax083/anon-bot">
+   
+  </a>
+  <a href="https://github.com/Dax083/anon-bot">
+  </a>
+</p>  
+
+<p align="center">
+<img src="https://telegra.ph/file/de3e9d2a021c2ac516b12.jpg">
+<p>
+
+
+Ini adalah repo untuk random chat bot atau biasa di kenal sebagai Anonymous Chat
+
+<b>
+<a [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy) /></a>
+</b>
+### VIA VPS
+
+```console
+$ git clone https://github.com/Dax083/anon-bot
+$ cd anon-bot
+$ pip3 install -U -r requirements.txt
+$ nano config.py
+$ screen -S anon-bot
+$ python3 ChatBot.py
+```
+
+### TEMUKAN KAMI DI TELEGRAM
+
+<a href="https://t.me/kang_culiknew"><img src="https://img.shields.io/badge/OWNER -blue?style=for-the-badge&logo=Telegram" /></a>
+<a href="https://t.me/vvslh_pro"><img src="https://img.shields.io/badge/SUPPORT GROUP-black?style=for-the-badge&logo=Telegram" /></a>
+
+---
+### ✔️ FITUR SUPPORT BOT 
+
+- [✓] TEXT
+- [✓] FOTO
+- [✓] VIDEO
+- [✓] STICKER
+- [✓] PESAN SUARA
+- [✓] PESAN TERUSAN
+---
+## CREDIT
+* [](https://github.com/Dax083/anon-bot)
+Thanks for all development 🙏
+
